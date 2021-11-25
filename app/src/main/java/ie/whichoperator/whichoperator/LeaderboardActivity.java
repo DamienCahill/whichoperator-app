e50a09b7d7107fe956f08d1d12b6cf18f887097d
DamienCahill <53192581+DamienCahill@users.noreply.github.com>
2021-11-25 21:46:07 +0000

    starting creating the leaderboard screen

--- app/src/main/java/ie/whichoperator/whichoperator/LeaderboardActivity.java
+++ app/src/main/java/ie/whichoperator/whichoperator/LeaderboardActivity.java
@@ -1,9 +1,12 @@
 package ie.whichoperator.whichoperator;
 
+import android.annotation.SuppressLint;
 import android.os.Build;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
 
 
 public class LeaderboardActivity extends AppCompatActivity {
@@ -12,5 +15,16 @@ public class LeaderboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         client = new LeaderBoardClient(BuildConfig.USERNAME, BuildConfig.PASSWORD, BuildConfig.URL);
+    }
+
+    @SuppressLint("NewApi")
+    public void populateLeaderboard() {
+        LeaderboardPlayer[] leaderboard;
+        try {
+            leaderboard = client.getLeaderboard();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
